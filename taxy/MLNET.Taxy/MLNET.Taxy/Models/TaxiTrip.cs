97a017ec5b9afa9e058e8e655fc0b27e6895eb7f
Victor Bilokin <victor.bilokin@outlook.com>
2018-07-29 00:36:49 +0300

    init structure commit

---
+++ taxy/MLNET.Taxy/MLNET.Taxy/Models/TaxiTrip.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MLNET.Taxy.Models
+{
+    public class TaxiTrip
+    {
+    }
+}
---
+++ taxy/MLNET.Taxy/MLNET.Taxy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MLNET.Taxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
