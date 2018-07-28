dd9f24c6adc60c0303959ad8bd6be646c1ab3460
Victor Bilokin <victor.bilokin@outlook.com>
2018-07-29 01:32:31 +0300

    completed example

--- taxy/MLNET.Taxy/MLNET.Taxy/Models/TaxiTrip.cs
+++ taxy/MLNET.Taxy/MLNET.Taxy/Models/TaxiTrip.cs
@@ -1,10 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿
+using Microsoft.ML.Runtime.Api;
 
 namespace MLNET.Taxy.Models
 {
     public class TaxiTrip
     {
+        [Column("0")]
+        public string VendorId;
+
+        [Column("1")]
+        public string RateCode;
+
+        [Column("2")]
+        public float PassengerCount;
+
+        [Column("3")]
+        public float TripTime;
+
+        [Column("4")]
+        public float TripDistance;
+
+        [Column("5")]
+        public string PaymentType;
+
+        [Column("6", "Label")]
+        public float FareAmount;
     }
 }
---
+++ taxy/MLNET.Taxy/MLNET.Taxy/Models/TaxiTripFarePrediction.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.ML.Runtime.Api;
+
+namespace MLNET.Taxy.Models
+{
+    public class TaxiTripFarePrediction
+    {
+        [ColumnName("Score")]
+        public float FareAmount;
+    }
+}
